7fcb7dc2fc9eebb11a815af19676fadd3db40e06
chandg <chandg@tcd.ie>
2018-07-02 18:34:06 +0100

    PacketIn updated

--- src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
+++ src/main/java/net/floodlightcontroller/mactracker/MACTracker.java
@@ -7,12 +7,24 @@ import net.floodlightcontroller.core.IFloodlightProviderService;
 import java.util.ArrayList;
 import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.Set;
+
+import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.TCP;
+import net.floodlightcontroller.packet.UDP;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.TransportPort;
+import org.projectfloodlight.openflow.types.VlanVid;
 
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
@@ -85,7 +97,7 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
 	
 	@Override
 	   public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-	        Ethernet eth =
+	       Ethernet eth =
 	                IFloodlightProviderService.bcStore.get(cntx,
 	                                            IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 	 
@@ -96,7 +108,77 @@ public class MACTracker implements IOFMessageListener, IFloodlightModule {
 	                    eth.getSourceMACAddress().toString(),
 	                    sw.getId().toString());
 	        }
-	        return Command.CONTINUE;
+	        
+	        
+	        
+	        
+		
+		 switch (msg.getType()) {
+		    case PACKET_IN:
+		        /* Retrieve the deserialized packet in message */
+		       // Ethernet eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
+		 
+		        /* Various getters and setters are exposed in Ethernet */
+		        MacAddress srcMac = eth.getSourceMACAddress();
+		        VlanVid vlanId = VlanVid.ofVlan(eth.getVlanID());
+		 
+		        /* 
+		         * Check the ethertype of the Ethernet frame and retrieve the appropriate payload.
+		         * Note the shallow equality check. EthType caches and reuses instances for valid types.
+		         */
+		        if (eth.getEtherType() == EthType.IPv4) {
+		            /* We got an IPv4 packet; get the payload from Ethernet */
+		            IPv4 ipv4 = (IPv4) eth.getPayload();
+		             
+		            /* Various getters and setters are exposed in IPv4 */
+		            byte[] ipOptions = ipv4.getOptions();
+		            IPv4Address dstIp = ipv4.getDestinationAddress();
+		            System.out.println("Source IP : " + ipv4.getSourceAddress() + "  Destination IP : " + dstIp);
+		            		             
+		            /* 
+		             * Check the IP protocol version of the IPv4 packet's payload.
+		             */
+		            if (ipv4.getProtocol() == IpProtocol.TCP) {
+		                /* We got a TCP packet; get the payload from IPv4 */
+		            	
+		                TCP tcp = (TCP) ipv4.getPayload();
+		  
+		                /* Various getters and setters are exposed in TCP */
+		                TransportPort srcPort = tcp.getSourcePort();
+		                TransportPort dstPort = tcp.getDestinationPort();	                
+		                short flags = tcp.getFlags();
+		                
+		                 
+		                /* Your logic here! */
+		                
+		            } else if (ipv4.getProtocol() == IpProtocol.UDP) {
+		                /* We got a UDP packet; get the payload from IPv4 */
+		                UDP udp = (UDP) ipv4.getPayload();
+		  
+		                /* Various getters and setters are exposed in UDP */
+		                TransportPort srcPort = udp.getSourcePort();
+		                TransportPort dstPort = udp.getDestinationPort();
+		                 
+		                /* Your logic here! */
+		            }
+		 
+		        } else if (eth.getEtherType() == EthType.ARP) {
+		            /* We got an ARP packet; get the payload from Ethernet */
+		            ARP arp = (ARP) eth.getPayload();
+		 
+		            /* Various getters and setters are exposed in ARP */
+		            boolean gratuitous = arp.isGratuitous();
+		 
+		        } else {
+		            /* Unhandled ethertype */
+		        }
+		        break;
+		    default:
+		        break;
+		    }
+		
+		 
+		 return Command.CONTINUE;
 	    }
 
 	@Override
